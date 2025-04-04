998bc9fc33ab11c5ee9821ec2510b75e5c0b38e9
Sheepslayer666 <48016217+Sheepslayer666@users.noreply.github.com>
2025-04-04 17:01:52 +0200

    minor bugfixes

--- generator.py
+++ generator.py
@@ -12,46 +12,57 @@ porpuse = input("npc or villian?: ")
 
 if alligment_ply == "good":
     random.shuffle(utilites.ALLIGNMENT_GOOD)
-    print(utilites.ALLIGNMENT_GOOD[0])
+    selected_allignment = utilites.ALLIGNMENT_GOOD[0]
 elif alligment_ply == "evil":
     random.shuffle(utilites.ALLIGNMENT_EVIL)
-    print(utilites.ALLIGNMENT_EVIL[0])
+    selected_allignment = utilites.ALLIGNMENT_EVIL[0]
 elif alligment_ply == "neutral":
     random.shuffle(utilites.ALLIGNMENT_NEUT)
-    print(utilites.ALLIGNMENT_NEUT[0])
+    selected_allignment = utilites.ALLIGNMENT_NEUT[0]
 else:
     print("Sorry this alligment is not supported")
 
 def name_genrator():
+    surename = str(random.choice(utilites.surenames_1)) + str(random.choice(utilites.surenames_2))
     if spices_ply == "human":
         givenname = random.choice(utilites.human_given_name)
-        surename = str(random.choice(utilites.surenames_1)) + str(random.choice(utilites.surenames_2))
     elif spices_ply == "elf":
         givenname = random.choice(utilites.elf_given_name)
-        surename = str(random.choice(utilites.surenames_1)) + str(random.choice(utilites.surenames_2))
     elif spices_ply == "dwarf":
         givenname = random.choice(utilites.dwarf_given_name)
-        surename = str(random.choice(utilites.surenames_1)) + str(random.choice(utilites.surenames_2))
 
-    if alligment_ply == "evil" or porpuse == "villian":
-        fullname = givenname + " the worldeater"
+    if alligment_ply == "evil":
+        fullname = givenname + random.choice(utilites.evil_titles)
         print(fullname)
-    elif alligment_ply == "neutral" or porpuse == "npc":
+    elif alligment_ply == "neutral":
         fullname =givenname + " " + surename
         print(fullname)
     elif alligment_ply == "good":
-        fullname = givenname + " the heavenly"
+        fullname = givenname + random.choice(utilites.good_titles)
         print(fullname)
-
-fullname = name_genrator()
-
-def backgroung_generator():
     if alligment_ply == "evil":
         print(f"{fullname}" + " was feared")
     elif alligment_ply == "neutral":
         print(f"{fullname}" + " was okay, i mean okay notring special")
     elif alligment_ply == "neutral":
         print(f"{fullname}" + " was a wonderful person")
+    return fullname
+
+
+def backgroung_generator():
+    flaw_ply = random.choice(utilites.FLAWS)
+    bonds_ply = random.choice(utilites.BONDS)
+    person_ply = random.sample(utilites.PERSONALITY_TRAIS, 2)
+    print(selected_allignment)
+    if "chaotic" in selected_allignment:
+        ideals_ply = utilites.IDEALS.get("chaotic")
+        print(ideals_ply)
+    if "neutral" in selected_allignment:
+        ideals_ply = utilites.IDEALS.get("neutral")
+        print(ideals_ply)
+    if "good" in selected_allignment:
+        ideals_ply = utilites.IDEALS.get("good")
+        print(ideals_ply)
 
 name_genrator()
 backgroung_generator()
--- utilites.py
+++ utilites.py
@@ -1,11 +1,32 @@
 STANDARD_ARRAY = [15, 14, 13, 12, 10, 8]
 ALLIGNMENT_GOOD = ["Lawful good", "Neutral good", "Chaotic good"]
-ALLIGNMENT_EVIL = ["Lawful neutral", "True Neutral", "Chaotic neutral"]
-ALLIGNMENT_NEUT = ["Lawful evil", "Neutral evil", "Neutral evil"]
+ALLIGNMENT_EVIL = ["Lawful evil", "Neutral evil", "Neutral evil"]
+ALLIGNMENT_NEUT = ["Lawful neutral", "True Neutral", "Chaotic neutral"]
 SKILLS = ["strenght", "dexterity", "intelligence", "wisdom", "charisma"]
 SPECIES = ["Dwarf", "Elf", "Halfling", "Human"]
+PERSONALITY_TRAIS = ["I always have a story from a faraway place.", "I observe more than I speak, but when I do, people listen.", "I see potential friends in everyone I meet.", 
+"I keep a mental map of every place I visit.", "I live for the moment—planning is for others.", "I carry a token from someone I left behind and think of them often.",
+"I’m suspicious of those who stay in one place too long.", "I journal everything—someone will want to read it one day." ]
+IDEALS = {
+    "chaotic" : "Freedom",
+    "neutral" : "Knowledge",
+    "good" : "Hope",
+    None : "Self-Reliance", 
+    None : "Adaptability",
+
+}
+FLAWS = ["I find it hard to stay in one place or with one group for too long.", "I embellish my stories… a lot.", 
+"I trust too easily—or not at all.", "I avoid conflict, even when I should stand my ground.", "I’m haunted by memories of a past I try to forget."]
+BONDS = ["I owe a great debt to someone who once saved my life.", "I’m searching for a place that truly feels like home."
+, "I made a promise to someone and intend to keep it, no matter how far I roam.", "My journal contains the only record of a lost civilization.", "I once failed someone who depended on me, and I won’t let that happen again."]
 human_given_name = ["Frederic", "Goldwyn", "Bob", "Billard", "Rudolf", "Bloodwyn", "Gomez"]
 dwarf_given_name = ["Olin", "Brewin", "Dalin", "Thorin", "Thorlof", "Arnulf"]
 elf_given_name = ["Dalion", "Tylion", "Quillion", "Cyndrion"]
 surenames_1 = ["Iron", "Horse", "Tree", "Beer", "Wolf", "Bow", "Light", "Fire", "Oak"]
-surenames_2 = ["master", "caster", "killer", "foe", "hand", "frined", "traveller"]
+surenames_2 = ["master", "caster", "killer", "foe", "hand", "frined", "traveller", "walker", "eater"]
+evil_titles = [" the destroyer", " the wordeater", " the dreadful", " the imp"]
+good_titles = [" the goodwilling", " the heavenly", " the great", " hero of the west"]
+
+background_gener = """You’ve spent your life drifting from place to place, picking up odds and ends of knowledge and skill.
+ When you come to a new town, you can usually find lodging and modest work in exchange for stories, songs, or simple services. 
+ Locals are often willing to share information or assistance, trusting in your adaptable nature."""
