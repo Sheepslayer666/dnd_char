3f904933495cf66faa106710043f659468d2e6e0
Sheepslayer666 <48016217+Sheepslayer666@users.noreply.github.com>
2025-04-07 13:44:16 +0200

    Add files via upload
    minimal change on the combat file

--- combat.py
+++ combat.py
@@ -1,12 +1,12 @@
-import utilites
 import dice
 import generator
 import enemies
 
 hit = None
-is_combat = None
+is_combat = False
 
-if dice.d20.roll() + generator.modifyer_set["strenght"]  > enemies.Goblin["AC"]:
-    hit = True
-else:
-    hit = False
+while is_combat == True:
+    if dice.d20.roll() + generator.modifyer_set["strenght"]  > enemies.Goblin["AC"]:
+        hit = True
+    else:
+        hit = False
--- enemies.py
+++ enemies.py
@@ -1,3 +1,10 @@
 Goblin = {
-    "AC" : 15
+    "AC" : 15,
+    "damage" : 6,
+    "distance" : None,
+}
+Hog = {
+    "AC" : 10,
+    "damage" : 5,
+    "distance" : None,
 }
