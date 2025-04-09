d68d477da175b2aa87510e3aa4282a995871d5a4
Sheepslayer666 <48016217+Sheepslayer666@users.noreply.github.com>
2025-04-09 19:06:25 +0200

    Add files via upload

--- combat.py
+++ combat.py
@@ -1,9 +1,17 @@
 import dice
-import generator
+#import generator
 import enemies
+import player
+import math
 
 hit = None
 is_combat = False
+position_ply = [1]
+postion_enemy = [0]
+
+while math.dist(position_ply, postion_enemy) <= 10.0:
+    is_combat = True
+
 
 while is_combat == True:
     if dice.d20.roll() + generator.modifyer_set["strenght"]  > enemies.Goblin["AC"]:
