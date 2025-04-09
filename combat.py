1368e217167a4e76f05c97b13b68dc0ac77f9dd7
Sheepslayer666 <48016217+Sheepslayer666@users.noreply.github.com>
2025-04-09 19:19:53 +0200

    Add files via upload

--- combat.py
+++ combat.py
@@ -8,10 +8,10 @@ hit = None
 is_combat = False
 position_ply = [1]
 postion_enemy = [0]
+distance = math.dist(position_ply, postion_enemy)
 
-while math.dist(position_ply, postion_enemy) <= 10.0:
-    is_combat = True
-
+while distance <= 10.0:
+    is_combat is True
 
 while is_combat == True:
     if dice.d20.roll() + generator.modifyer_set["strenght"]  > enemies.Goblin["AC"]:
